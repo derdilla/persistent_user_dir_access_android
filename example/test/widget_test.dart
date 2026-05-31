173125dcf7d591c547240b57c86bfac68fae94c9
derdilla <82763757+derdilla@users.noreply.github.com>
2026-06-01 00:07:59 +0200

    Compatibility with flutter 3.44 (#2)
    * Update plugin to flutter 3.44
    
    * Update example
    
    * Fix example compilation
    
    * Run dart format
    
    * Change CI channel to stable
    
    * Remove PR trigger
    
    * Update workflows

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:persistent_user_dir_access_android/persistent_user_dir_access_android.dart';
 
-import 'package:persistent_user_dir_access_android_example/main.dart';
+import 'package:example/main.dart';
 
 void main() {
   testWidgets('Builds sample controls', (WidgetTester tester) async {
@@ -13,9 +13,11 @@ void main() {
     expect(find.text('requestDirectoryUri()'), findsOneWidget);
     expect(find.text('Selected directory: null'), findsOneWidget);
     expect(
-        find.text(
-            "writeFile(uri, 'test.txt', 'text/plain', utf8.encode('Test text'))"),
-        findsOneWidget);
+      find.text(
+        "writeFile(uri, 'test.txt', 'text/plain', utf8.encode('Test text'))",
+      ),
+      findsOneWidget,
+    );
     expect(find.byType(ListTile), findsNWidgets(2));
   });
 
@@ -49,19 +51,24 @@ void main() {
     expect(plugin.methodInvokeHistory, hasLength(1));
     expect(plugin.methodInvokeHistory[0], 'requestDirectoryUri');
 
-    await tester.tap(find.text(
-        "writeFile(uri, 'test.txt', 'text/plain', utf8.encode('Test text'))"));
+    await tester.tap(
+      find.text(
+        "writeFile(uri, 'test.txt', 'text/plain', utf8.encode('Test text'))",
+      ),
+    );
     await tester.pump();
     expect(plugin.methodInvokeHistory, hasLength(2));
-    expect(plugin.methodInvokeHistory[1],
-        'writeFile(test://uri,test.txt,text/plain, [84, 101, 115, 116, 32, 116, 101, 120, 116])');
+    expect(
+      plugin.methodInvokeHistory[1],
+      'writeFile(test://uri,test.txt,text/plain, [84, 101, 115, 116, 32, 116, 101, 120, 116])',
+    );
   });
 }
 
 class MockPersistentUserDirAccessAndroid
     implements PersistentUserDirAccessAndroid {
   final List<String> methodInvokeHistory = [];
-  String? sampleUri = null;
+  String? sampleUri;
   bool writeFileResult = true;
 
   @override
@@ -72,7 +79,11 @@ class MockPersistentUserDirAccessAndroid
 
   @override
   Future<bool> writeFile(
-      String dirUri, String fileName, String mimeType, Uint8List data) async {
+    String dirUri,
+    String fileName,
+    String mimeType,
+    Uint8List data,
+  ) async {
     methodInvokeHistory.add('writeFile($dirUri,$fileName,$mimeType, $data)');
     return writeFileResult;
   }
